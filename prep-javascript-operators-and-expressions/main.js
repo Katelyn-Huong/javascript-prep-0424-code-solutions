7be6d1e0a269c29e5dc531f57ad9b6d47e603a2b
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-04-25 20:22:03 +0000

    Completed prep-javascript-operators-and-expressions.

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 1;
+const number2 = 2;
+const product = number1 * number2;
+console.log('value of product:', product);
+console.log('typeof product:', typeof product);
+
+const charge = 10;
+const payment = 5;
+const amountRemaining = charge - payment;
+console.log('value of amountRemaining:', amountRemaining);
+console.log('typeof amountRemaining:', typeof amountRemaining);
+
+const tests = 6;
+const assignments = 7;
+const final = 11;
+const grade = (6 + 7 + 11) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Katelyn';
+const lastName = 'Ngo';
+const fullName = 'Katelyn' + ' Ngo';
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 10;
+const isAcidic = 10 > 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const numberOfSoldiers = 15;
+const isSparta = 15 === '300';
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let nickname = 'Katelyn' + ' Ngo';
+nickname += ' is the GOAT';
+console.log('value of nickname:', nickname);
+console.log('typeof nickname:', typeof nickname);
